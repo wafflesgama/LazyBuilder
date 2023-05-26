5e026cd613d4511e0b656f96b2af89985caee692
Guilherme Gama <guilherme.s.gam@gmail.com>
2023-05-26 02:40:47 +0100

    Point Gizmo Adapts To Scene camera distance

--- Assets/LazyProcedural/Scripts/Gizmos/PointGizmo.cs
+++ Assets/LazyProcedural/Scripts/Gizmos/PointGizmo.cs
@@ -2,16 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 namespace LazyProcedural.Gizmos
 {
     [ExecuteAlways]
     public class PointGizmo : MonoBehaviour
     {
-        public float pointSize = 0.2f;
+        public float pointSize = 0.04f;
+        public float pointSizeGrowDistance = 0.015f;
         private void OnDrawGizmos()
         {
             UnityEngine.Gizmos.color = Color.cyan;
-            UnityEngine.Gizmos.DrawSphere(transform.position, pointSize);
+
+#if UNITY_EDITOR
+            UnityEngine.Gizmos.DrawSphere(transform.position, pointSize + (pointSizeGrowDistance * (Vector3.Distance(transform.position, SceneView.currentDrawingSceneView.camera.transform.position))));
+#else
+   UnityEngine.Gizmos.DrawSphere(transform.position, pointSize);
+#endif
         }
     }
 }
